39c663db7dc91e999fe80a479b1dc45a36db9210
AdamRohdell <rohdell@student.chalmers.se>
2019-11-21 10:03:30 +0100

    fixed lab 2_A

--- Lab2_A/BlackJack.hs
+++ Lab2_A/BlackJack.hs
@@ -13,7 +13,7 @@ hand2 = Add (Card (Numeric 2) Hearts)
 sizeSteps :: [Integer]
 sizeSteps = [size hand2, size (Add (Card (Numeric 2) Hearts)(Add (Card Jack Spades) Empty)),
              size (Add (Card Jack Spades) Empty) + 1, 
-             size (Empty + 2), 
+             size Empty + 2, 
              2]
 
 -- A nice way to print out a Card.
