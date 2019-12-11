70c54863241393e3ab19e58cde25a23e9e97a995
AdamRohdell <rohdell@student.chalmers.se>
2019-12-11 15:06:35 +0100

    Working on Lab4 need to fix functions
    Co-authored-by: amandadehlen <dehlena@student.chalmers.se>

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Parsing
-
+import Data.Char
+import Data.Maybe
 
 data Expr = Num Double
           | Var String
@@ -8,9 +9,9 @@ data Expr = Num Double
           | Mul Expr Expr
           | Sin Expr
           | Cos Expr
-          deriving (Eq)
+          deriving (Eq, Show)
 
-type Name = String
+type Name = String  
 
 sin' = Prelude.sin
 cos' = Prelude.cos
@@ -28,6 +29,7 @@ mul = Mul
 sin,cos :: Expr -> Expr
 sin = Sin
 cos = Cos 
+
 
 
 showExpr :: Expr -> String
@@ -49,14 +51,78 @@ eval (Sin e) d     = sin' (eval e d)
 eval (Cos e) d     = cos' (eval e d)
 
 
+expr, term, factor :: Parser Expr
+expr = foldl1 Add <$> chain term (char '+')
+term = foldl1 Mul <$> chain factor (char '*')
+factor = Num <$> number <|> do char '(' *> expr <* char ')' 
+                <|> Sin <$> sinus <|> Cos <$> cosinus
+
+number :: Parser Double
+number = read <$> oneOrMore digit
+
+addition :: Parser Double
+addition = operation '+' (+)
+
+multiplication :: Parser Double
+multiplication = operation '*' (*)
+
+sinus :: Parser Expr
+sinus = do
+        char 's'
+        char 'i'
+        char 'n'
+        char '('
+        n <- expr
+        char ')'
+        return n
+
+cosinus :: Parser Expr
+cosinus = do
+        char 'c'
+        char 'o'
+        char 's'
+        char '('
+        n <- expr
+        char ')'
+        return n
+
+operation :: Char -> (Double -> Double -> b) -> Parser b
+operation c op = do
+        n <- number
+        char c --sat (=='+') s
+        m <- number
+        return (m `op` n)
+        
+calculation :: Parser Double
+calculation = addition <|> multiplication
+
+{-
+num' :: String -> Maybe(Int, String)
+num' str = case span isDigit str of 
+            ((c:cs),rest) -> Just (read (c:cs), rest)
+            _             -> Nothing
 
 
-readExpr :: String -> Maybe Expr
+additionParse :: String -> Maybe(Int, String)
+additionParse n = case num' n of 
+            Just (d, '+':r) -> case num' r of
+                                    Just (m, r') -> Just(d+m, r')
+                                    Nothing      -> Nothing
+            _               -> Nothing
+-}
+{-readExpr :: String -> Maybe Expr
 readExpr str = parse parser str
 
             
             where parser = Parser Num
-    
+    -}
 
 
+readEvalPrint = do
+        putStr "What would you like to calculate?"
+        s <- getLine
+        let s' = filter (not.isSpace) s
+        case parse expr s' of
+                Just (ans,"") -> print (eval ans 0)
+                _             -> putStrLn "invalid expression"
 
