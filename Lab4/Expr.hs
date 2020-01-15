cf9340b34e096456cda3364114cea2bb08359029
AdamRohdell <rohdell@student.chalmers.se>
2020-01-15 17:35:39 +0100

    Finished lab 4, cant fix last prop

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -11,8 +11,8 @@ import Data.String
 data Operator = Add|Mul
         deriving (Eq)
 instance Show Operator where
-        show Add = "+"
-        show Mul = "*" 
+        show Add = "Add"
+        show Mul = "Mul" 
 
 type FunctionName = String
         
@@ -21,13 +21,10 @@ data Expr = Num Double
           | Var Char
           | Operation Operator Expr Expr
           | Function FunctionName Expr
---          | Add Expr Expr
---          | Mul Expr Expr
---          | Sin Expr
---          | Cos Expr
-          deriving (Eq, Show)
+          deriving  (Show)
 
-
+instance Eq Expr where
+        (==) e1 e2 = abs((eval e1 1) - (eval e2 1)) < 0.000001
 
 instance Arbitrary Expr where
     arbitrary = sized arbExpr
@@ -57,8 +54,17 @@ cos = Function "Cos"
 showExpr :: Expr -> String
 showExpr (Num d)     = show d
 showExpr (Var s)     = s:""
-showExpr (Operation op e1 e2) = showExpr e1 ++ show op ++ showExpr e2
-showExpr (Function f e)     = showFun (Function f e) ++ showExpr e
+showExpr (Operation Mul e1 e2) = showFactor e1 ++ showOp Mul ++ showFactor e2
+showExpr (Operation op e1 e2) = showExpr e1 ++ showOp op ++ showExpr e2
+showExpr (Function f e)     = showFun (Function f e) ++ "(" ++ showExpr e ++ ")"
+
+showFactor :: Expr -> String
+showFactor (Num n) = show n
+showFactor e = "(" ++ showExpr e ++ ")"
+
+showOp :: Operator -> String
+showOp Mul = "*"
+showOp Add = "+"
 
 showFun :: Expr -> String
 showFun (Function "Cos" e) = "Cos "
@@ -98,18 +104,24 @@ multiplication = operation '*' (*)
 mathFunc :: FunctionName -> Parser Expr
 mathFunc f = do 
              mapM_ char f
-             n <- expr
-             return n
+             parsingWithParenthesis <|> Var <$> char 'x' <|> Num <$> number
+
+parsingWithParenthesis :: Parser Expr
+parsingWithParenthesis = do
+                         char '('
+                         n <- expr
+                         char ')'
+                         return n
 
 operation :: Char -> (Double -> Double -> b) -> Parser b
 operation c op = do
         n <- number
-        char c  
+        char c
         m <- number
         return (m `op` n)
         
 calculation :: Parser Double
-calculation = addition <|> multiplication
+calculation = multiplication <|> addition
 
 readExpr :: String -> Maybe Expr
 readExpr str = case parse expr (filter (not.isSpace) str) of
@@ -119,10 +131,10 @@ readExpr str = case parse expr (filter (not.isSpace) str) of
 
 
 prop_ShowReadExpr :: Expr -> Bool
-prop_ShowReadExpr e1
-                |  isNothing $ readExpr(showExpr e1) = False
-                |  readExpr(showExpr e1) == Just e1  = True
-                |  otherwise                         = False
+prop_ShowReadExpr e =
+        case readExpr (showExpr e) of
+                Just a -> e == a
+                Nothing -> error "bad input xd"
 
 
 arbExpr :: Int -> Gen Expr
@@ -130,7 +142,7 @@ arbExpr 0 = arbNum
 arbExpr n = frequency [(5, arbFun (arbExpr (n-1))), (4, do
                                                     m <- choose (0, n-1)
                                                     arbBin (arbExpr m) (arbExpr (n-1-m))),
-                        (1, return (Var 'x'))]
+                        (0, return (Var 'x'))]
 
 arbFun :: Gen Expr -> Gen Expr
 arbFun e = do e1 <- e
@@ -143,7 +155,19 @@ arbBin e1 e2 = do e1' <- e1
 
 arbNum :: Gen Expr
 arbNum = Num <$> suchThat arbitrary (>0.1)
-        
+
+simplify' :: Expr -> Expr
+simplify' (Operation Add (Operation Add (Var 'x') (Num n1)) (Num n2)) = 
+                Operation Add (Var 'x') (Num (n1+n2))
+simplify' (Operation Add (Operation Add (Num n1) (Var 'x')) (Num n2)) = 
+                Operation Add (Var 'x') (Num (n1+n2))
+simplify' (Operation Mul (Operation Mul (Var 'x') (Num n1)) (Num n2)) = 
+                Operation Mul (Var 'x') (Num (n1*n2))
+simplify' (Operation Mul (Operation Mul (Num n1) (Var 'x')) (Num n2)) = 
+                Operation Mul (Var 'x') (Num (n1*n2))                
+simplify' e = e
+
+
 simplify :: Expr -> Expr
 simplify (Operation Mul (Num 0) _)         = Num 0
 simplify (Operation Mul _ (Num 0))         = Num 0
@@ -160,13 +184,13 @@ simplify (Operation Mul e1 (Num 1))        = simplify e1
 
 simplify (Operation Add e1 e2)
         | e1 /= e1' || e2 /=e2' = simplify $ Operation Add (simplify e1) (simplify e2)
-        | otherwise             = Operation Add (simplify e1) (simplify e2)
+        | otherwise             = simplify' (Operation Add e1 e2)--Operation Add e1 e2
                 where   e1' = simplify e1
                         e2' = simplify e2
 
 simplify (Operation Mul e1 e2)  
         | e1 /= e1' || e2 /=e2' = simplify $ Operation Mul (simplify e1) (simplify e2)
-        | otherwise             = Operation Mul (simplify e1) (simplify e2)
+        | otherwise             = simplify' (Operation Mul e1 e2)
                 where   e1' = simplify e1
                         e2' = simplify e2  
 simplify e                      = e
